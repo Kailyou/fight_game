b6f776dc3528f582c92224280213dfb608771370
Michael Zimmermann <sigmaepsilon92@gmail.com>
2015-08-28 12:08:27 +0200

    limit number of players to 2

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -4,24 +4,10 @@ using UnityEngine.Networking;
 
 public class CustomNetworkManager : NetworkManager {
 
-	/*public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
-	{
-		GameObject player = (GameObject)Instantiate(playerPrefab, CustomGetStartPosition(), Quaternion.identity);
-		//player.GetComponent<Player>().color = Color.Red;
-		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
-	}
-
-	public Transform CustomGetStartPosition() {
-		Debug.Log ("lol", gameObject);
-
-		NetworkStartPosition[] positions = gameObject.GetComponents<NetworkStartPosition> ();
-		for(int i=0; i<positions.Length; i++) {
-			NetworkStartPosition pos = positions[i];
-			Debug.Log(pos.name);
-			if(pos.name=="spawn_local")
-				return pos.;
+	public override void OnServerConnect(NetworkConnection conn) {
+		if (numPlayers >= 2) {
+			conn.Disconnect();
+			return;
 		}
-
-		return null;
-	}*/
+	}
 }
